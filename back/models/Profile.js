723c92fd3ff0b9e7f788c39f91251c5429aad8d0
Nam Koong Kwon <rndrnjs2003@gmail.com>
2020-07-21 17:14:46 +0900

    feat : [back] Add Model and Route about Profile
    ? Model
    - use mongoose
    - The column can have a list because it is NoSQL.
    ? Route
    - Validation using express-Validator.
    - Chapter3 studying
    ? 출처
    - https://www.udemy.com/course/mern-stack-front-to-back/learn/lecture/10055148#content

---
+++ back/models/Profile.js
@@ -0,0 +1,109 @@
+const mongoose = require("mongoose");
+
+const ProfileSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.SchemaTypes.ObjectId,
+    ref: 'user',
+  },
+  company: {
+    type: String,
+    required: true,
+  },
+  website: {
+    type: String,
+  },
+  location: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: String,
+  },
+  skills: {
+    type: [String],
+  },
+  bio: {
+    type: String,
+  },
+  githubusername: {
+    type: String,
+  },
+  exprerience: [{
+    title: {
+      type: String,
+      required: true
+    },
+    company: {
+      type: String,
+      required: true,
+    },
+    location: {
+      type: String,
+      required: true,
+    },
+    from: {
+      type: Date,
+      required: true,
+    },
+    to: {
+      type: Date,
+    },
+    current: {
+      type: Boolean,
+      default: false,
+    },
+    description: {
+      type: String
+    }
+  }],
+  deucation: [{
+    school: {
+      type: String,
+      required: true
+    },
+    degree: {
+      type: String,
+      required: true,
+    },
+    fieldofstudy: {
+      type: String,
+    },
+    from: {
+      type: Date,
+      required: true,
+    },
+    to: {
+      type: Date,
+    },
+    current: {
+      type: Boolean,
+      default: false,
+    },
+    description: {
+      type: String
+    }
+  }],
+  social: [{
+    youtube: {
+      type: String,
+    },
+    twitter: {
+      type: String,
+    },
+    facebook: {
+      type: String,
+    },
+    linkedin: {
+      type: String
+    },
+    instagram: {
+      type: String
+    },
+  }],
+  date: {
+    type: Date,
+    default: Date.now
+  },
+})
+
+module.exports = Profile = mongoose.model('profile', ProfileSchema);
