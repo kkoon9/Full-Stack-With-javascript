0b1d6498812c7f63fc6513732b7ad0404b24e0cc
Nam Koong Kwon <rndrnjs2003@gmail.com>
2020-08-13 00:32:42 +0900

    feat[FRONT] : Redux Setup & Alerts
    - redux actions, alert에 대해 공부

--- front/src/components/auth/Register.js
+++ front/src/components/auth/Register.js
@@ -1,7 +1,10 @@
 import React, { Fragment, useState } from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { setAlert } from '../../actions/alert';
+import PropTypes from 'prop-types';
 
-const Register = () => {
+const Register = ({ setAlert }) => {
   const [formData, setFormData] = useState({
     name: "",
     email: "",
@@ -16,7 +19,7 @@ const Register = () => {
   const onSubmit = async e => {
     e.preventDefault();
     if (password !== confirm_password) {
-      console.log('Password do not match');
+      setAlert('Password do not match', 'danger', 3000);
     } else {
       console.log("Success");
       // const newUser = {
@@ -72,4 +75,8 @@ const Register = () => {
   );
 };
 
-export default Register;
+Register.propTypes = {
+  setAlert: PropTypes.func.isRequired
+}
+
+export default connect(null, { setAlert })(Register);
