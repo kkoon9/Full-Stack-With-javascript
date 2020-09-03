57dbd86ca4545c6c0f7431b7ebc58dde1fa816c2
kkoon9 <rndrnjs2003@gmail.com>
2020-09-03 21:06:20 +0900

    feat : 구구단 게임 구현
    - state에 대한 이해
    - onSubmit이 끝나고 새로고침되는 이슈
    - e.preventDefault()를 통해 해결

--- front/src/components/game/Game.js
+++ front/src/components/game/Game.js
@@ -1,6 +1,5 @@
 import React, { Fragment } from 'react';
 import { Link } from 'react-router-dom';
-import Multiplication from './Multiplication';
 
 const Game = () => {
   return (
--- front/src/components/game/Multiplication.js
+++ front/src/components/game/Multiplication.js
@@ -1,16 +1,61 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import { Link } from 'react-router-dom';
-import Multiplication from './Multiplication';
 
-const Game = () => {
+const Multiplication = () => {
+  const [gugudanState, setGugudanState] = useState({
+    first: Math.ceil(Math.random() * 9),
+    second: Math.ceil(Math.random() * 9),
+    value: '',
+    result: ''
+  });
+  const onChange = (e) => {
+    setGugudanState({
+      ...gugudanState,
+      value: e.target.value
+    })
+  }
+  const onSubmit = (e) => {
+    //e.preventdefault();
+    if (gugudanState.first * gugudanState.second === parseInt(gugudanState.value)) {
+      setGugudanState({
+        ...gugudanState,
+        value: '',
+        result: "정답입니다!"
+      })
+    } else {
+      setGugudanState({
+        ...gugudanState,
+        value: "",
+        result: "땡"
+      })
+    }
+    e.preventDefault();
+  }
+  const onClick = (e) => {
+    setGugudanState({
+      first: Math.ceil(Math.random() * 9),
+      second: Math.ceil(Math.random() * 9),
+      value: "",
+      result: ""
+    })
+  }
   return (
     <Fragment>
       <h2>이곳은 구구단 게임장입니다.</h2>
+      <div>{gugudanState.first} 곱하기 {gugudanState.second}는?</div>
+      <form onSubmit={(e) => onSubmit(e)}>
+        <input type="number" value={gugudanState.value} onChange={(e) => onChange(e)} />
+        <button>입력!</button>
+      </form>
+      <div>{gugudanState.result}</div>
+      <button className="btn btn-light" onClick={(e) => onClick(e)}>
+        다음문제
+      </button>
       <Link to="/Game" className="btn btn-light">
         이전으로
       </Link>
-    </Fragment>
+    </Fragment >
   );
 };
 
-export default Game;
+export default Multiplication;
