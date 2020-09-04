467cbe68c16dc5e8ba70bfaa9602fb8b840f8b86
kkoon9 <rndrnjs2003@gmail.com>
2020-09-04 12:10:33 +0900

    fix : 새로고침 해결
    -     e.preventDefault()로 적어야 하는데     e.preventdefault()f로 적어서 오타로 인해 새로고침이 계속 된거 였음!

--- front/src/components/game/Multiplication.js
+++ front/src/components/game/Multiplication.js
@@ -1,53 +1,43 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment, useState, useRef } from 'react';
 import { Link } from 'react-router-dom';
 
 const Multiplication = () => {
-  const [gugudanState, setGugudanState] = useState({
-    first: Math.ceil(Math.random() * 9),
-    second: Math.ceil(Math.random() * 9),
-    value: '',
-    result: ''
-  });
+  const [first, setFirst] = useState(Math.ceil(Math.random() * 9));
+  const [second, setSecond] = useState(Math.ceil(Math.random() * 9));
+  const [value, setValue] = useState('');
+  const [result, setResult] = useState('');
   const onChange = (e) => {
-    setGugudanState({
-      ...gugudanState,
-      value: e.target.value
-    })
+    setValue(e.target.value);
   }
   const onSubmit = (e) => {
-    //e.preventdefault();
-    if (gugudanState.first * gugudanState.second === parseInt(gugudanState.value)) {
-      setGugudanState({
-        ...gugudanState,
-        value: '',
-        result: "정답입니다!"
-      })
-    } else {
-      setGugudanState({
-        ...gugudanState,
-        value: "",
-        result: "땡"
-      })
-    }
     e.preventDefault();
+    if (first * second === parseInt(value)) {
+      setFirst(Math.ceil(Math.random() * 9));
+      setSecond(Math.ceil(Math.random() * 9));
+      setValue('');
+      setResult("정답입니다");
+    } else {
+      setValue('');
+      setResult("땡");
+    }
+    inputRef.current.focus();
   }
   const onClick = (e) => {
-    setGugudanState({
-      first: Math.ceil(Math.random() * 9),
-      second: Math.ceil(Math.random() * 9),
-      value: "",
-      result: ""
-    })
+    setFirst(Math.ceil(Math.random() * 9));
+    setSecond(Math.ceil(Math.random() * 9));
+    setValue("");
+    setResult("");
   }
+  const inputRef = useRef();
   return (
     <Fragment>
       <h2>이곳은 구구단 게임장입니다.</h2>
-      <div>{gugudanState.first} 곱하기 {gugudanState.second}는?</div>
+      <div>{first} 곱하기 {second}는?</div>
       <form onSubmit={(e) => onSubmit(e)}>
-        <input type="number" value={gugudanState.value} onChange={(e) => onChange(e)} />
+        <input ref={inputRef} value={value} onChange={(e) => onChange(e)} />
         <button>입력!</button>
       </form>
-      <div>{gugudanState.result}</div>
+      <div>{result}</div>
       <button className="btn btn-light" onClick={(e) => onClick(e)}>
         다음문제
       </button>
